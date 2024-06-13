e27a32ad5e19a765aaf7660e8595fabd0dd04ac6
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-06-13 22:10:11 +0300

    add filter files

---
+++ frontend/src/app/shared/article-filter/article-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'article-filter',
+  templateUrl: './article-filter.component.html',
+  styleUrls: ['./article-filter.component.scss']
+})
+export class ArticleFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
